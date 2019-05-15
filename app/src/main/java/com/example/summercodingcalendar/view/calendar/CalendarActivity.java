d62b6f12e920af8ac5a9d90ca34463ca5aa9cf4e
JaytenJeon <jayten0821@gmail.com>
2019-05-15 12:08:11 +0900

    feat: MVP 패턴을 적용한 Splash Activity 추가

--- app/src/main/java/com/example/summercodingcalendar/view/calendar/CalendarActivity.java
+++ app/src/main/java/com/example/summercodingcalendar/view/calendar/CalendarActivity.java
@@ -5,13 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.summercodingcalendar.R;
-import com.example.summercodingcalendar.databinding.ActivityMainBinding;
+import com.example.summercodingcalendar.databinding.ActivityCalendarBinding;
 
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        ActivityCalendarBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_calendar);
     }
 }
---
+++ app/src/main/java/com/example/summercodingcalendar/view/splash/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.summercodingcalendar.view.splash;
+
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.summercodingcalendar.R;
+import com.example.summercodingcalendar.databinding.ActivitySplashBinding;
+import com.example.summercodingcalendar.view.calendar.CalendarActivity;
+
+public class SplashActivity extends AppCompatActivity implements SplashContract.View {
+    private SplashPresenter mSplashPresenter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivitySplashBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
+        mSplashPresenter = new SplashPresenter(this);
+        mSplashPresenter.onStart();
+    }
+
+    @Override
+    public void navigateToCalendarActivity() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), CalendarActivity.class));
+                finish();
+            }
+        }, 1300);
+    }
+}
