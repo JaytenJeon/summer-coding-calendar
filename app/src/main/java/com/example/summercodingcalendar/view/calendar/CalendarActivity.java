b7c6f75d2fe85f0501285833397d882ca17b646c
JaytenJeon <jayten0821@gmail.com>
2019-05-15 11:49:52 +0900

    refactor: 패키지 디렉토리 구조 변경

--- app/src/main/java/com/example/summercodingcalendar/view/calendar/CalendarActivity.java
+++ app/src/main/java/com/example/summercodingcalendar/view/calendar/CalendarActivity.java
@@ -1,12 +1,13 @@
-package com.example.summercodingcalendar;
+package com.example.summercodingcalendar.view.calendar;
 
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.summercodingcalendar.R;
 import com.example.summercodingcalendar.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
